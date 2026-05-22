4b54f0c1d7d5fb0c4a128a3a0676d2a06c630569
Narasimha Reddy <49887403+narasimhareddyprostack@users.noreply.github.com>
2026-05-22 19:34:37 +0530

    Ts

--- class-9-Emp-CRUD-MongoDB/routes/empRouter.js
+++ class-9-Emp-CRUD-MongoDB/routes/empRouter.js
@@ -3,6 +3,32 @@ import EmployeeModel from '../models/Employee.js';
 let router = express.Router();
 
 /*
+usage: create new employee
+Rest API URL: http://127.0.0.1:8080/emp/create
+Method Type:POST
+Request Fields: eid,ename,esal 
+Access Type: Public  prostack@gmail.com
+*/
+router.post("/create",async(req,resp)=>{
+    try {
+        let employee=req.body;
+        console.log(employee);//3
+        let emp_obj=await EmployeeModel.findOne({"eid":employee.eid})
+        console.log(emp_obj)
+        if(emp_obj){
+                return resp.status(404).json({"msg":"Employee already Exists"})
+        }
+        emp_obj=new EmployeeModel(employee)
+        console.log(emp_obj) //4
+        emp_obj=await emp_obj.save();
+        return resp.status(200).json({"msg":"New Emplyee Created successfully "})        
+
+    } catch (error) {
+         return resp.status(404).json({"msg":"Unable to create"})
+    }
+})
+
+/*
 usage: fetch all employees
 Rest API URL: http://127.0.0.1:8080/emp/read
 Method Type:GET 
